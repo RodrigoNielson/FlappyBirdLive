4877c0968abb58408d5cf903f290c1964419d39f
RodrigoNielson <digo_nielson@hotmail.com>
2019-11-24 18:02:37 -0300

    flappy

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    public float upForce = 200f;
+
+    private bool isDead = false;
+
+    private Animator animator;
+    private Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                animator.SetTrigger("Flap");
+
+                rigidbody2d.velocity = Vector2.zero;
+                rigidbody2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+        rigidbody2d.velocity = Vector2.zero;
+        animator.SetTrigger("Die");
+        GameControl.instance.BirdDied();
+    }
+}
